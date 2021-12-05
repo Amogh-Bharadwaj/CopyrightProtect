60b7c375216f514e48dab6e0df55dd5d18cdde03
[[REDACTED]] <52661649+heavycrystal@users.noreply.github.com>
2021-12-05 14:33:17 +0530

    express nonsense #1

--- app/auth.js
+++ app/auth.js
@@ -4,17 +4,20 @@ var app = express()
 var CryptoJS = require("crypto-js");
 var sqlite3 = require('sqlite3').verbose();
 
- 
- 
+let db = new sqlite3.Database('production.db');
 
-app.use(cors())
+ app.use(cors())
 app.use(express.json())
 app.listen(5000, () => console.log(`Listening on port 5000`)); 
  
 app.post('/auth', (req, res) => { 
   const username = req.body.name;
   const password = CryptoJS.SHA256(req.body.password);
-  //run().then(pass => res.send({ express: 'happys password: '+ password }));
-  res.send({ express: 'happys password: '+ password });
-  
+
+  db.run('INSERT INTO USERS(ACCOUNT_ID, USERNAME, PASSWORD) VALUES(?, ?, ?)', ["penis", username, password], (err) => {
+	if(err) {
+		return console.log(err.message); 
+	}
+	console.log('Row was added to the table: ${this.lastID}');
+    })
 }); 
